4e1f91a0f9a4170ba2f6390bb6808afdb82936f8
Group1 <hongpink473@gmail.com>
2024-08-28 14:55:16 +0700

    ComputerShop

--- src/main/java/com/example/customershopbackend/entities/invoice/feture/dto/InvoiceRequest.java
+++ src/main/java/com/example/customershopbackend/entities/invoice/feture/dto/InvoiceRequest.java
@@ -10,6 +10,12 @@ public record InvoiceRequest(
         @NotNull(message = "Owe Amount is required.")
         @PositiveOrZero(message = "Owe Amount must be Positive number.")
         BigDecimal oweAmount,
+        @NotNull(message = "Paid Amount is required.")
+        @PositiveOrZero(message = "Paid Amount must be Positive number.")
+        BigDecimal paidAmount,
+        @NotNull(message = "Total Amount is required.")
+        @PositiveOrZero(message = "Total Amount must be Positive number.")
+        BigDecimal totalAmount,
         @NotBlank(message = "Customer Phone is required.")
         String customerPhone
 ) {
